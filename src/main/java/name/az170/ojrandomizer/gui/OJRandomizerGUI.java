d539ac7207f0fb745fdb2a22bae74ad33e2cd40e
chenzhen <zhen.chen@cn.iisigroup.com>
2016-08-16 21:20:18 +0800

    update

---
+++ src/main/java/name/az170/ojrandomizer/gui/OJRandomizerGUI.java
@@ -0,0 +1,141 @@
+package name.az170.ojrandomizer.gui;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+import name.az170.ojrandomizer.CharaResult;
+import name.az170.ojrandomizer.OJRandomizer;
+
+public class OJRandomizerGUI extends Application {
+    
+    private static Logger logger = LogManager.getLogger();
+
+    private OJRandomizer ojRandomizer = new OJRandomizer();
+    
+    private BorderPane root;
+    private GridPane grid;
+    private ToolBar toolBar;
+    private ImageView[][] images = new ImageView[2][6];
+    private HBox[][] hBoxes = new HBox[2][6];
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        root = new BorderPane();
+        
+        initToolBar();
+        initGrid();
+        
+        Scene scene = new Scene(root, 1000, 450);
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("OJRandomizer");
+        primaryStage.show();
+    }
+    
+    private void initToolBar() {
+        Button randomAllBtn = new Button("All");
+        Button randomCharaBtn = new Button("Chara");
+        Button randomCardsBtn = new Button("Cards");
+
+        randomAllBtn.setOnAction(new RandomAllEventHandler());
+        randomCharaBtn.setOnAction(new RandomCharaEventHandler());
+        randomCardsBtn.setOnAction(new RandomCardsEventHandler());
+
+        toolBar = new ToolBar(randomAllBtn, randomCharaBtn, randomCardsBtn);
+        root.setTop(toolBar);
+    }
+    
+    private void initGrid() {
+        grid = new GridPane();
+        
+        Image image = new Image(getClass().getResourceAsStream("qp.jpg"));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 6; j++) {
+                hBoxes[i][j] = new HBox();
+                hBoxes[i][j].setAlignment(Pos.CENTER);
+                images[i][j] = new ImageView();
+                images[i][j].setImage(image);
+                images[i][j].fitWidthProperty().bind(root.widthProperty().divide(6));
+                images[i][j].fitHeightProperty().bind(root.heightProperty().subtract(toolBar.heightProperty()).divide(2));
+                images[i][j].setPreserveRatio(true);
+                hBoxes[i][j].getChildren().add(images[i][j]);
+                grid.add(hBoxes[i][j], j, i);
+            }
+        }
+        
+        for (int i = 0; i < 6; i++) {
+            ColumnConstraints columnConstraints = new ColumnConstraints();
+            columnConstraints.setPercentWidth(1.0 / 6 * 100);
+            grid.getColumnConstraints().add(columnConstraints);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setPercentHeight(50.0);
+            grid.getRowConstraints().add(rowConstraints);
+        }
+        
+        root.setCenter(grid);
+    }
+    
+    private void randomAll() {
+        randomChara();
+        randomCards();
+    }
+    
+    private void randomChara() {
+        CharaResult result = ojRandomizer.randomizeChara();
+        logger.info(result.getChara().getCharaName());
+    }
+    
+    private void randomCards() {
+        // TODO
+    }
+    
+    public class RandomAllEventHandler implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent event) {
+            randomAll();
+        }
+        
+    }
+    
+    public class RandomCharaEventHandler implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent event) {
+            randomChara();
+        }
+        
+    }
+    
+    public class RandomCardsEventHandler implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent event) {
+            randomCards();
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
