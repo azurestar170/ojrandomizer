351e87df218d5ef6005cf7802e90ae69bcf94416
chenzhen <zhen.chen@cn.iisigroup.com>
2016-08-19 18:17:54 +0800

    update

---
+++ src/main/java/name/az170/ojrandomizer/gui/CharaTable.java
@@ -0,0 +1,13 @@
+package name.az170.ojrandomizer.gui;
+
+import javafx.scene.control.TableView;
+
+public class CharaTable extends TableView<CharaConfigRow> {
+
+    public CharaTable() {
+        super();
+        setEditable(true);
+        
+    }
+
+}
